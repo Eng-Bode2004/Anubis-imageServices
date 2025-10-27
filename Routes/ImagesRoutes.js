27256fa5d86098010f744d71694b4c85d3dfad2b
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-10-27 21:24:30 +0300

    init commit

---
+++ Routes/ImagesRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const ImagesControllers = require('../Controllers/ImagesControllers');
+
+
+
+                            // Upload User Profile Image //
+
+const upload = require('../Middlewares/Multer-Users-ProfileImage'); // parser
+router.post('/profile-image', upload.single('image'), ImagesControllers.uploadPhoto);
+
+
+
+
+module.exports = router;
