e5f9f5a864469785b145153ccf2bcbf1025ec006
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-10-28 03:59:40 +0300

    cors

--- app.js
+++ app.js
@@ -2,14 +2,19 @@
 require('./Config/DataBase');
 const express = require('express');
 
-
+const cors = require('cors');
 // Set Up Port and Make Server listen To requests
 const app = express();
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 app.use(express.json()); // Middleware to parse JSON
 
-
+// ✅ حل مشكلة CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 // Main Routes
 //  Image Routes //
